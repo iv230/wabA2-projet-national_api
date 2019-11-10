1fa94381abd3fa94b0430b05ce12a15c856b0155
lucas <ID+username@users.noreply.github.com>
2019-11-10 19:33:35 +0100

    Support for all routes

--- seed.sql
+++ seed.sql
@@ -1,14 +1,17 @@
 USE my_database_1;
 
+DROP TABLES IF EXISTS users;
+
 CREATE TABLE IF NOT EXISTS users (
     `id` INT NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(50) NOT NULL,
     `email` VARCHAR(50) NOT NULL,
     `passwordHash` VARCHAR(50) NOT NULL,
+    `school` INT(4) NOT NULL DEFAULT 0,
+    `role` INT(4) NOT NULL DEFAULT 0,
     CONSTRAINT user_pk PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 INSERT INTO users (`name`, `email`, `passwordHash`)
 VALUES ('Admin1', 'admin1@exemple.com', '$2y$10$yRryXxcXaRJ4Azw76wzNt.po12nAXqPF3oPJqyyBfxktz3QoenM6y'),
        ('Admin2', 'admin2@exemple.com', '$2y$10$yRryXxcXaRJ4Azw76wzNt.po12nAXqPF3oPJqyyBfxktz3QoenM6y');
-       
--- server.js
+++ server.js
@@ -1,25 +1,49 @@
-console.log("Starting Node.JS server...");
-
 // -------------------------------
-// Program Initialisation
+// Requires
 // -------------------------------
 
-var http = require('http');
 var express = require('express');
+var bodyParser = require("body-parser");
 var mysql = require('mysql');
 
-var app = express();
-let port = 8080;
+// -------------------------------
+// Global Variabmes
+// -------------------------------
 
+var hostname = 'localhost';
+var port = 8080;
 let errorMsg = '[{\'data\' = \'null\'}]';
-let requestConsoleMsg = function(method, url, id, content) {
-    return "============================" +
-           "\nGot HTTP request:" +
-           "\n    Medthod: " + method +
-           "\n    URL: " + url +
-           (id ? "\n    ID: " + id : "") +
-           (content ? "\n    Content: " + content : "")
-}
+let successMsg = '[{\'success\' = \'true\'}]'
+let badRequestMsg = '[{\'badRequest\' = \'true\'}]'
+
+
+// -------------------------------
+// Main objects
+// -------------------------------
+
+var app = express();
+var myRouter = express.Router();
+
+
+// -------------------------------
+// Global Functions
+// -------------------------------
+
+let requestConsoleMsg = function (method, url, content) {
+    return "========================================================" +
+        "\nGot HTTP request:" +
+        "\n    Medthod: " + method +
+        "\n    URL: " + url +
+        (content ? "\n    Content: " + JSON.stringify(content) : "")
+};
+
+
+// -------------------------------
+// Body parser
+// -------------------------------
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 
 // -------------------------------
@@ -27,18 +51,18 @@ let requestConsoleMsg = function(method, url, id, content) {
 // -------------------------------
 
 let dbConnexionData = {
-    host       : 'database-nodejs',
-    port       : '3306',
+    host: 'database-nodejs',
+    port: '3306',
     //socketPath : '/var/run/mysqld/mysqld.sock',
-    user       : 'my_user_1',
-    password   : 'my_password_1',
-    database   : 'my_database_1'
+    user: 'my_user_1',
+    password: 'my_password_1',
+    database: 'my_database_1'
 };
 
 let db = mysql.createConnection(dbConnexionData);
 
-setTimeout(function() {
-    db.connect(function(err) {
+setTimeout(function () {
+    db.connect(function (err) {
         if (err) {
             console.error('Error connecting to MySQL: ' + err.stack);
             return;
@@ -49,65 +73,120 @@ setTimeout(function() {
 
 
 // -------------------------------
-// Request management
+// Route and Application Core
 // -------------------------------
 
-// GET method routes
-app.get('/', function(req, res) {
-    console.log(requestConsoleMsg("GET", req.get('host') + req.originalUrl));
-
-    db.query("SELECT * FROM users", function (err, result) {
-        res.send(!err ? result : errorMsg)
+myRouter.route('/')
+    .all(function (req, res) {
+        res.json({
+            message: "National BDE API", methode: req.method
+        });
     });
-});
 
-app.get('/:id', function(req, res) {
-    let id = req.params.id;
+myRouter.route('/users')
 
-    console.log(requestConsoleMsg("GET", req.get('host') + req.originalUrl, id));
+    .get(function (req, res) {
+        console.log(requestConsoleMsg("GET", req.get('host') + req.originalUrl));
 
-    db.query("SELECT * FROM users WHERE id=" + id, function (err, result) {
-        res.send(!err ? result : errorMsg)
+        let condition = "";
+
+        if (req.query.id) {
+            condition = " WHERE id=" + req.query.id;
+        } else if (req.query.email) {
+            condition = " WHERE email=\'" + req.query.email + "\'";
+        }
+
+        let request = "SELECT * FROM users" + condition;
+        console.log(request);
+
+        db.query(request, function (err, result) {
+            res.json(!err ? result : 'Unable to answer request')
+        });
+    })
+
+    .post(function (req, res) {
+        console.log(requestConsoleMsg("POST", req.get('host') + req.originalUrl, req.body));
+
+        let name = req.body.name;
+        let email = req.body.email;
+        let passwordHash = req.body.passwordHash;
+        let school = req.body.school;
+        let role = req.body.role;
+
+        if (name != null && name != undefined &&
+            email != null && email != undefined &&
+            passwordHash != null && passwordHash != undefined
+        ) {
+            let request = "INSERT INTO users (name, email, passwordHash, school, role)" +
+                ` VALUES ('${name}', '${email}', '${passwordHash}', '${school}', '${role}')`
+
+            console.log(request);
+
+            db.query(request, function (err, result) {
+                res.json(!err ? sucessMsg : errorMsg)
+                if (err) { console.log("[WARN] MySQL error: " + err) }
+            });
+        } else {
+            console.log('Failed to POST: ' + `${name}, ${email}, ${passwordHash}, ${school}, ${role}`);
+            res.json('Cannot create user while at least one value is missing');
+        }
+    })
+
+    .put(function (req, res) {
+        console.log(requestConsoleMsg("PUT", req.get('host') + req.originalUrl, req.body));
+
+        let id = req.query.id;
+
+        if (!id) {
+            res.json('Cannot update user while no ID is specified');
+        } else {
+            let request = "UPDATE users SET ";
+
+            let keys = Object.keys(req.body);
+
+            if (keys.length >= 1) {
+
+                keys.map((key, index) => {
+                    request += `\`${key}\` = "${req.body[key]}"` + (index < keys.length - 1 ? ', ' : '')
+                });
+
+                request = request + ` WHERE id=${id}`;
+
+                console.log(request);
+
+                db.query(request, function (err, result) {
+                    res.json(!err ? successMsg : errorMsg)
+                    if (err) { console.log("[WARN] MySQL error: " + err) }
+                });
+            } else {
+                res.json('Cannot update user while no values to change');
+            }
+        }
+    })
+
+    .delete(function (req, res) {
+        console.log(requestConsoleMsg("DELETE", req.get('host') + req.originalUrl, req.body));
+
+        let id = req.query.id;
+
+        if (id) {
+            let request = `DELETE FROM users WHERE id=${id}`;
+
+            db.query(request, function (err, result) {
+                res.json(!err ? successMsg : errorMsg);
+            })
+        } else {
+            res.json('Cannot delete user while no ID is specified');
+        }
     });
-});
-
-// POST method route
-app.post('/', function (req, res) {
-    console.log(requestConsoleMsg("POST", req.get('host') + req.originalUrl, null, req.body.variable_name));
-    res.send('not implemented');
-});
-
-// PUT method route
-app.put('/', function (req, res) {
-    console.log(requestConsoleMsg("POST", req.get('host') + req.originalUrl));
-    res.send(errorMsg);
-});
-
-app.put('/:id', function (req, res) {
-    let id = req.params.id;
-
-    console.log(requestConsoleMsg("POST", req.get('host') + req.originalUrl, id));
-    res.send('not implemented');
-});
-
-// DELETE method route
-app.delete('/', function (req, res) {
-    console.log(requestConsoleMsg("DELETE", req.get('host') + req.originalUrl));
-    res.send(errorMsg);
-});
-
-app.delete('/:id', function (req, res) {
-    let id = req.params.id;
-
-    console.log(requestConsoleMsg("DELETE", req.get('host') + req.originalUrl, id));
-    res.send('not implemented');
-});
 
 
 // -------------------------------
-// Starting listening
+// Starting Application
 // -------------------------------
 
-app.listen(port, function() {
-    console.log('Server is listening on ' + port)
+app.use(myRouter);
+
+app.listen(port, function () {
+    console.log("Running Node.JS Server on http://" + hostname + ":" + port);
 });
